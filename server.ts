3dfb44cc155ee1edac159ec7cd82b6cf71cdc922
tminh-U <tueminh192@outlook.com>
2026-04-24 21:48:20 +0700

    change gemini model

--- server.ts
+++ server.ts
@@ -141,7 +141,7 @@ Text to translate:
 }`;
 
     const aiResponse = await genAI.models.generateContent({
-      model: "gemini-2.5-flash",
+      model: "gemini-2.5-flash-lite",
       contents: prompt,
       config: {
         responseMimeType: "application/json",
